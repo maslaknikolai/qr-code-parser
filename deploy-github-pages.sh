90adfcd20ff5c8f745f48f2556ae042de7d18d0b
Nikolay Maslak <nikolay.maslak@onlinesup.com>
2020-11-01 23:01:54 +0300

    build: update bash deploy

--- deploy-github-pages.sh
+++ deploy-github-pages.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git branch -D gh-pages
+git co -b gh-pages
+
 yarn build
 
 rm -rf ./docs
@@ -7,5 +10,8 @@ rm -rf ./docs
 mv dist docs
 
 git add .
+git commit -m "UPDATE"
 
-git commit -m "UPDATE DOCS"
+git push origin gh-pages --force
+
+git co master
