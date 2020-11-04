e51d51f1837e3683feb5ea2127568f59c1b18788
Nikolay Maslak <nikolay.maslak@onlinesup.com>
2020-11-04 13:44:59 +0300

    dev: lint vue-html indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,16 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/html-indent': [
+      "error",
+      2,
+      {
+        "attribute": 1,
+        "baseIndent": 1,
+        "closeBracket": 0,
+        "alignAttributesVertically": true,
+        "ignores": []
+      }
+    ]
   },
 };
