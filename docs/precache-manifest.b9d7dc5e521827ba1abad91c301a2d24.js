01c5eab35b3a9baaa19ccfd3265dc29110fabd60
Nikolay Maslak <nikolay.maslak@onlinesup.com>
2020-11-06 00:51:25 +0300

    UPDATE

---
+++ docs/precache-manifest.b9d7dc5e521827ba1abad91c301a2d24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c6573753fd76414a49289865e69fb4",
+    "url": ".well-known/assetlinks.json"
+  },
+  {
+    "revision": "7eb2b348b15b2fe17998",
+    "url": "css/app.d317c90a.css"
+  },
+  {
+    "revision": "0fda1cff42e604f603df2a2a17d78d3f",
+    "url": "icon.png"
+  },
+  {
+    "revision": "354c34c8e18272d7f5c831b9ccc60be0",
+    "url": "index.html"
+  },
+  {
+    "revision": "7eb2b348b15b2fe17998",
+    "url": "js/app.147092e4.js"
+  },
+  {
+    "revision": "98130a2ea7243e96b047",
+    "url": "js/chunk-vendors.404a5c05.js"
+  },
+  {
+    "revision": "0578b2e5b820faa8c494a0642a537eb5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b4a169d0ce625909423d2e58fe46aee9",
+    "url": "qr-scanner-worker.min.js"
+  },
+  {
+    "revision": "105b58daea451d2688fd1e70366acfd8",
+    "url": "qr.example.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
